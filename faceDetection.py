60527cbd4f606e62adb09d3044f8f9546fb196ce
Vidya Sagar Jha <kingvsj@gmail.com>
2020-09-07 21:19:40 +0530

    second change

--- faceDetection.py
+++ faceDetection.py
@@ -4,7 +4,7 @@ faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.x
 img = cv2.imread("Resources/vsj.jpg") #just copy the image path here
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    #converting image to gray scale
 
-faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+faces = faceCascade.detectMultiScale(imgGray, 1.1, 13)
 
 for(x,y,w,h) in faces:
     cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
